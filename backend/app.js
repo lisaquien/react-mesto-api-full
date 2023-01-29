88dbf80c00324718cf68177177fd379f87a67e4d
Elizaveta Kruchinina <milky0105way@gmail.com>
2023-01-29 05:23:17 +0300

    ПР15 v2: изменяет адрес подключения к бд

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
 
